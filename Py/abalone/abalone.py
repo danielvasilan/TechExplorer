b30502a94223ddfd7c2c52f0f4702cd9a851085c
danielvasilan <daniel_vasilan@yahoo.com>
2020-05-05 15:30:57 +0300

    pc switch commit

---
+++ Py/abalone/abalone.py
@@ -0,0 +1,35 @@
+import pygame
+pygame.init()
+
+# create a class to draw a single cell
+def AbaloneGame():
+    
+    def ___init__(self):
+        self.SIZE = 5
+
+    def draw:
+        
+
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
